9b7344e8888ef24dd50a1d89e6eadb9acba01284
Celeste Senoseain <csenoseain@ms.local>
2016-09-28 14:17:39 -0300

    Add chrome like browser

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.config = {
   seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
   specs: ['spec.js'],
   capabilities: {
-    browserName: 'firefox'
+    browserName: 'chrome'
   }
 }
